5cc57ceeac476331546ac0f10192929a629fa58e
danishalikhan6888 <95619235+danishalikhan6888@users.noreply.github.com>
2021-12-14 16:59:37 +0500

    detail page

--- pages/detail.js
+++ pages/detail.js
@@ -7,25 +7,32 @@ import About from '../components/About';
 import Footer from '../components/Footer';
 import { Container, Row, Col } from 'reactstrap';
 
-const Index = () => {
+const Index = ({ data }) => {
   return (
     <Layout pageTitle="Landing Page Nextjs">
       <Header />
       <Container>
-    <Row>
-        <Col lg={12}>
-          <h2>Detail Page</h2>
-        </Col>
-      </Row>
-    </Container>
-     <Service />
-      <About />  
-       {/* <Hero /> */}
-      <Feature/>
-    
+        <Row>
+          <Col lg={12}>
+            <h2>Detail Page</h2>
+          </Col>
+        </Row>
+      </Container>
+      <Hero data={data} />
       <Footer />
     </Layout>
   )
 }
 
+
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://alpha-squad-uploads.s3.amazonaws.com/posts.json`)
+  const data = await res.json()
+  // Pass data to the page via props
+  return {
+    props: { data }
+  };
+}
 export default Index;
